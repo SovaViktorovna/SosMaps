6c7815614098fefc688a5d24c71e4b3979e1fae2
Viktoria Demchenko <sovaviktorovna430@gmail.com>
2025-03-07 09:30:48 +0400

    Initial Commit

---
+++ SosMapsTests/SosMapsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SosMapsTests.swift
+//  SosMapsTests
+//
+//  Created by Виктория Демченко on 07.03.25.
+//
+
+import Testing
+@testable import SosMaps
+
+struct SosMapsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
