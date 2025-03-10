55bcc3a7cfc051915a03c743523b30557d49e1df
Viktoria Demchenko <sovaviktorovna430@gmail.com>
2025-03-10 10:40:27 +0400

    An application has been created, a MapKit map has been added, zoom in, zoom out buttons have been added

--- SosMaps/ViewController.swift
+++ SosMaps/ViewController.swift
@@ -6,14 +6,100 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
-
+    
+    private let incrementButton = UIButton(type: .system)
+    private let decrementButton = UIButton(type: .system)
+    private var value = 0
+    var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupStepper()
+        
+        setConstraints()
     }
+    
+    private func setupStepper() {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.layer.cornerRadius = 4
+        stackView.clipsToBounds = true
+        stackView.layer.borderWidth = 2
+        stackView.layer.borderColor = UIColor.lightGray.cgColor
+        
 
+        
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        incrementButton.setTitle("+", for: .normal)
+        incrementButton.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        incrementButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        incrementButton.tintColor = .black
+        incrementButton.backgroundColor = .white
+        incrementButton.layer.borderWidth = 1
+        incrementButton.layer.borderColor = UIColor.lightGray.cgColor
+        incrementButton.addTarget(self, action: #selector(incrementTapped), for: .touchUpInside)
+        
+        decrementButton.setTitle("-", for: .normal)
+        decrementButton.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        decrementButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        decrementButton.tintColor = .black
+        decrementButton.backgroundColor = .white
+//        decrementButton.layer.borderColor = UIColor.black.cgColor
+        decrementButton.addTarget(self, action: #selector(decrementTapped), for: .touchUpInside)
+        
+        stackView.addArrangedSubview(incrementButton)
+        stackView.addArrangedSubview(decrementButton)
+        
+        print("StackView superview before adding: \(String(describing: stackView.superview))")
+        self.mapView.addSubview(stackView)
+        print("StackView superview after adding: \(String(describing: stackView.superview))")
+        
 
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: mapView.safeAreaLayoutGuide.topAnchor, constant: 16),
+            stackView.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 16)
+        ])
+        
+        stackView.frame = CGRect(x: 16, y: 50, width: 50, height: 120)
+        stackView.autoresizingMask = [.flexibleRightMargin, .flexibleBottomMargin]
+    }
+    
+    @objc private func incrementTapped() {
+        value += 1
+    }
+    
+    @objc private func decrementTapped() {
+        value -= 1
+    }
 }
+
+
+
+extension ViewController {
+    
+    func setConstraints() {
+        
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
+}
+
 
