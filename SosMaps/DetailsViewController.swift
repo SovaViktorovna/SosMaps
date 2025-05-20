2946c9b65ecbaf3b7a97c4d8d1336b84fa90082a
Viktoria Demchenko <sovaviktorovna430@gmail.com>
2025-05-20 11:26:29 +0400

    fixed the appearance of the button dismissButton

--- SosMaps/DetailsViewController.swift
+++ SosMaps/DetailsViewController.swift
@@ -16,6 +16,8 @@ class DetailsViewController: UIViewController {
         
         let dismissButton = UIButton(frame: CGRect(x: 20, y: 50, width: 100, height: 40))
         dismissButton.setTitle("Закрыть", for: .normal)
+        dismissButton.backgroundColor = .systemRed
+        dismissButton.layer.cornerRadius = 5
         dismissButton.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
         view.addSubview(dismissButton)
     }
